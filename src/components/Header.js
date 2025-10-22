b4c50b7822f6bfa889e5f0e3de88683f3bb69a9d
nikhil7476 <nikhil9027917476@gmail.com>
2025-10-22 20:17:42 +0530

    modified:   src/components/Footer.js 	modified:   src/components/Header.js 	modified:   src/pages/_app.js 	modified:   src/styles/globals.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,35 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function Header() {
-    return (
-        <Navbar collapseOnSelect expand="lg">
-            <Container className="m-4 mx-auto header-container">
-                {/* Brand aligned to left */}
-                <Navbar.Brand href="/" title="Chaska Food" className="logo">Chaska Food</Navbar.Brand>
-
-                {/* Toggle button for mobile */}
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
-                {/* Navigation links aligned to right */}
-                <Navbar.Collapse id="responsive-navbar-nav" className="menu justify-content-end">
-                    <Nav>
-                        <Nav.Link href="/" title='Home'>Home</Nav.Link>
-                        <Nav.Link href="#about" title='About'>About</Nav.Link>
-                        <Nav.Link href="/blog" title='Blog'>Blog</Nav.Link>
-                        <Nav.Link href="#contact" title='Contact'>Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar collapseOnSelect expand="lg" className="header-navbar">
+      <Container className="header-container my-3 mx-auto">
+        <Navbar.Brand href="/" title="Chaska Food" className="logo">
+          Chaska Food
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end"
+        >
+          <Nav className="menu">
+            <Nav.Link href="/" title="Home">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#about" title="About">
+              About
+            </Nav.Link>
+            <Nav.Link href="/blog" title="Blog">
+              Blog
+            </Nav.Link>
+            <Nav.Link href="#contact" title="Contact">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
